ebbe02303f22288e5bb900a0fe12b44077a0ff9a
Robert Setter <robert.setter@bobertdos.me>
2026-05-02 22:14:42 -0500

    Dapper params

--- FindYourHealth-backend/Controllers/FYHController.cs
+++ FindYourHealth-backend/Controllers/FYHController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using Microsoft.Data.SqlClient;
 using Dapper;
+using System.Data;
 
 namespace FindYourHealth_backend.Controllers
 {
@@ -25,7 +26,7 @@ namespace FindYourHealth_backend.Controllers
                 try
                 {
                     sqlConnection.Open();
-                    SqlCommand cmd = new SqlCommand() { Connection = sqlConnection };
+                    DynamicParameters dynamicParameters = new DynamicParameters();
                     StringBuilder builder1 = new StringBuilder(@"SELECT [Service Level]
       ,[Service Type]
       ,[Service]
@@ -49,71 +50,68 @@ namespace FindYourHealth_backend.Controllers
                     if (!string.IsNullOrEmpty(slevel))
                     {
                         builder1.Append("[Service Level] = @slevel AND ");
-                        cmd.Parameters.Add(new SqlParameter("slevel", slevel));
+                        dynamicParameters.Add("slevel", slevel);
                     }
                     if (!string.IsNullOrEmpty(stype))
                     {
                         builder1.Append("[Service Type] = @st AND ");
-                        cmd.Parameters.Add(new SqlParameter("st", stype));
+                        dynamicParameters.Add("st", stype);
                     }
                     if (!string.IsNullOrEmpty(srv))
                     {
                         builder1.Append("[Service] = @srv AND ");
-                        cmd.Parameters.Add(new SqlParameter("srv", srv));
+                        dynamicParameters.Add("srv", srv);
                     }
                     if (!string.IsNullOrEmpty(scat))
                     {
                         builder1.Append("[Service Category] = @scat AND ");
-                        cmd.Parameters.Add(new SqlParameter("scat", scat));
+                        dynamicParameters.Add("scat", scat);
                     }
                     if (!string.IsNullOrEmpty(sscat))
                     {
                         builder1.Append("[Service Subcategory] = @sscat AND ");
-                        cmd.Parameters.Add(new SqlParameter("sscat", sscat));
+                        dynamicParameters.Add("sscat", sscat);
                     }
                     if (!string.IsNullOrEmpty(agroup))
                     {
                         builder1.Append("[Age Group] = @agroup AND ");
-                        cmd.Parameters.Add(new SqlParameter("agroup", agroup));
+                        dynamicParameters.Add("agroup", agroup);
                     }
                     if (!string.IsNullOrEmpty(insc))
                     {
                         builder1.Append("[Insurance Company] = @insc AND ");
-                        cmd.Parameters.Add(new SqlParameter("insc", insc));
+                        dynamicParameters.Add("insc", insc);
                     }
                     if (!string.IsNullOrEmpty(insp))
                     {
                         builder1.Append("[Insurance Plan] = @insp AND ");
-                        cmd.Parameters.Add(new SqlParameter("insp", insp));
+                        dynamicParameters.Add("insp", insp);
                     }
                     if (!string.IsNullOrEmpty(comp))
                     {
                         builder1.Append("[Company] = @comp AND ");
-                        cmd.Parameters.Add(new SqlParameter("comp", comp));
+                        dynamicParameters.Add("comp", comp);
                     }
                     if (!string.IsNullOrEmpty(_cnty))
                     {
                         builder1.Append("[County] = @cnty AND ");
-                        cmd.Parameters.Add(new SqlParameter("cnty", _cnty));
+                        dynamicParameters.Add("cnty", _cnty);
                     }
                     if (!string.IsNullOrEmpty(sta))
                     {
                         builder1.Append("[State] = @sta AND ");
-                        cmd.Parameters.Add(new SqlParameter("sta", sta));
+                        dynamicParameters.Add("sta", sta);
                     }
                     if (!string.IsNullOrEmpty(language))
                     {
                         builder1.Append("[Language] = @lang AND ");
-                        cmd.Parameters.Add(new SqlParameter("lang", language));
+                        dynamicParameters.Add("lang", language);
                     }
                     string finalString = builder1.ToString();
                     int lastAND = finalString.LastIndexOf("AND ");
                     finalString = finalString.Remove(lastAND, 4);
                     _logger.LogInformation("Query to be used: " + finalString);
-                    cmd.CommandText = finalString;
-
-                    SqlDataReader dataReader = cmd.ExecuteReader();
-                    Affilitations = sqlConnection.Query<InsuranceAffiliationsModel>(finalString, cmd.Parameters);
+                    Affilitations = sqlConnection.Query<InsuranceAffiliationsModel>(finalString, dynamicParameters);
                 }
                 catch (Exception ex)
                 {
