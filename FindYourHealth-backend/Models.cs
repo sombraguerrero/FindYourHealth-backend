e0eed884d697cc4e83a60a426f16f16ae8fa3f31
Robert Setter <robert.setter@bobertdos.me>
2026-05-02 23:09:23 -0500

    default constructor

--- FindYourHealth-backend/Models.cs
+++ FindYourHealth-backend/Models.cs
@@ -34,6 +34,29 @@ namespace FindYourHealth_backend
         public string? Website { get; set; } = "";
         public string? Language { get; set; } = "";
 
+        public InsuranceAffiliationsModel()
+        {
+            ServiceLevel = "";
+            ServiceType = "";
+            Service = "";
+            ServiceCategory = "";
+            SubServiceCategory = "";
+            AgeGroup = "";
+            City = "";
+            State = "";
+            Zip = "";
+            Phone = "";
+            InsuranceCompany = "";
+            InsurancePlan = "";
+            Locations = "";
+            County = "";
+            Company = "";
+            Street = "";
+            SuiteBuilding = "";
+            Website = "";
+            Language = "";
+        }
+
         public InsuranceAffiliationsModel(string ServiceLevel, string ServiceType, string Service, string ServiceCategory, string SubServiceCategory, string AgeGroup, string InsuranceCompany, string InsurancePlan, string Company, string Locations, string County, string Street, string SuiteBuilding, string City, string State, string Zip, string Phone, string Website, string Language)
         {
             this.ServiceLevel = ServiceLevel;
