c604ff94ed8a12436be73a2517bc2a8324e5886f
Robert Setter <robert.setter@bobertdos.me>
2026-05-02 20:33:35 -0500

    null/case insensitivity

--- FindYourHealth-backend/Program.cs
+++ FindYourHealth-backend/Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.ConfigureHttpJsonOptions(o =>
+{
+    o.SerializerOptions.PropertyNameCaseInsensitive = true;
+});
+
 
 var app = builder.Build();
 
